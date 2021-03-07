90b9f3a924a9be07e163c368371dc2cba127d334
Jarek Decowski <jaroslaw.decowski@gmail.com>
2021-03-07 19:36:44 +0100

    deleted non-meaning comments

--- index.php
+++ index.php
@@ -8,6 +8,8 @@ include('config/db_connection.php');
     //displaying results from the table
     $users = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
+   
+
 mysqli_free_result($result);
 
 mysqli_close($con);
@@ -15,12 +17,11 @@ mysqli_close($con);
 
 
 
-
 ?>
-
 
 <!DOCTYPE html>
 <html>
+
 <?php include( 'templates/header.php' ); ?>
 
 
@@ -37,8 +38,12 @@ mysqli_close($con);
 
         
     <?php include( 'templates/footer.php' ); ?>
-</html>
 
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
 
 
 
